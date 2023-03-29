66d21e1492e015dea376ced1b80effa5cd5d67f7
Lucas Steuernagel <lucas.tnagel@gmail.com>
2023-03-29 17:50:57 -0300

    Add codegen test case

---
+++ tests/codegen_testcases/solidity/solana_accounts_validation.sol
@@ -0,0 +1,93 @@
+// RUN: --target solana --emit cfg
+
+contract test {
+
+    // BEGIN-CHECK: test::test::function::sum__int256_int256::preamble
+    // CHECK: block0: # entry
+	// CHECK: branchcond (unsigned more uint32 1 > (builtin ArrayLength ((builtin Accounts ())))), block2, block1
+    // CHECK: block1: # in_bounds
+	// CHECK: return
+    // CHECK: block2: # out_of_bounds
+	// CHECK: print (alloc string uint32 41 "An account is missing for the transaction")
+	// CHECK: assert-failure
+
+    @reader(acc1)
+    function sum(int256 a, int256 b) public view returns (int256) {
+        if (tx.accounts.acc1.is_writable) {
+            return a-b;
+        }
+        return a+b;
+    }
+
+    // BEGIN-CHECK: test::test::function::add__int256_int256::preamble
+    // CHECK: block0: # entry
+	// CHECK: branchcond (unsigned more uint32 1 > (builtin ArrayLength ((builtin Accounts ())))), block2, block1
+    // CHECK: block1: # in_bounds
+	// CHECK: branchcond (struct (subscript struct AccountInfo[] (builtin Accounts ())[uint32 0]) field 5), block3, block4
+    // CHECK: block2: # out_of_bounds
+	// CHECK: print (alloc string uint32 41 "An account is missing for the transaction")
+	// CHECK: assert-failure
+    // CHECK: block3: # account_0_validated
+	// CHECK: return
+    // CHECK: block4: # validation_0_failed
+	// CHECK: print (alloc string uint32 33 "Account \'acc1\' should be a signer")
+	// CHECK: assert-failure
+
+    @signer(acc1)
+    function add(int256 a, int256 b) public view returns (int256) {
+        if (tx.accounts.acc1.is_writable) {
+            return a-b;
+        }
+        return a+b;
+    }
+
+    // BEGIN-CHECK: test::test::function::add2__int256_int256::preamble
+    // CHECK: block0: # entry
+	// CHECK: branchcond (unsigned more uint32 2 > (builtin ArrayLength ((builtin Accounts ())))), block2, block1
+    // CHECK: block1: # in_bounds
+	// CHECK: branchcond (struct (subscript struct AccountInfo[] (builtin Accounts ())[uint32 0]) field 5), block3, block4
+    // CHECK: block2: # out_of_bounds
+	// CHECK: print (alloc string uint32 41 "An account is missing for the transaction")
+	// CHECK: assert-failure
+    // CHECK: block3: # account_0_validated
+	// CHECK: branchcond (struct (subscript struct AccountInfo[] (builtin Accounts ())[uint32 1]) field 5), block5, block6
+    // CHECK: block4: # validation_0_failed
+	// CHECK: print (alloc string uint32 33 "Account \'acc1\' should be a signer")
+	// CHECK: assert-failure
+    // CHECK: block5: # account_1_validated
+	// CHECK: return
+    // CHECK: block6: # validation_1_failed
+	// CHECK: print (alloc string uint32 32 "Account \'acc2\' should be mutable")
+	// CHECK: assert-failure
+
+    @signer(acc1)
+    @mutable(acc2)
+    function add2(int256 a, int256 b) public view returns (int256) {
+        if (tx.accounts.acc1.is_writable && tx.accounts.acc2.is_signer) {
+            return a-b;
+        }
+        return a+b;
+    }
+
+    // BEGIN-CHECK: test::test::function::add3__int256_int256::preamble
+    // CHECK: block0: # entry
+	// CHECK: branchcond (unsigned more uint32 1 > (builtin ArrayLength ((builtin Accounts ())))), block2, block1
+    // CHECK: block1: # in_bounds
+	// CHECK: branchcond ((struct (subscript struct AccountInfo[] (builtin Accounts ())[uint32 0]) field 5) & (struct (subscript struct AccountInfo[] (builtin Accounts ())[uint32 0]) field 5)), block3, block4
+    // CHECK: block2: # out_of_bounds
+	// CHECK: print (alloc string uint32 41 "An account is missing for the transaction")
+	// CHECK: assert-failure
+    // CHECK: block3: # account_0_validated
+	// CHECK: return
+    // CHECK: block4: # validation_0_failed
+	// CHECK: print (alloc string uint32 41 "Account \'acc3\' should be a mutable signer")
+	// CHECK: assert-failure
+
+    @mutableSigner(acc3)
+    function add3(int256 a, int256 b) public view returns (int256) {
+        if (tx.accounts.acc3.is_writable && tx.accounts.acc3.is_signer) {
+            return a-b;
+        }
+        return a+b;
+    }
+}
